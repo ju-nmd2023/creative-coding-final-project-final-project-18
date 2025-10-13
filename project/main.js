fb698fac955659be70778993a81e2a08dda397e7
filippahans <hanssonfilippa@hotmail.com>
2025-10-13 23:31:43 +0200

    test commit

--- project/main.js
+++ project/main.js
@@ -2,27 +2,21 @@ let centerX, centerY;
 let lastX = -999,
   lastY = -999;
 let drawing = false;
-let song;
-let songStarted = false;
-let amp;
-let stars = [];
-let lines = [];
-let smoothedLevel = 0;
 
-function preload() {
-  song = loadSound("assets/music/spaceymusic.mp3");
-}
+//array to store stars
+let stars = [];
+
+//array to store drawn lines
+let lines = [];
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('p5container');
+  createCanvas(windowWidth, windowHeight);
   centerX = width / 2;
   centerY = height / 2;
   colorMode(RGB, 255, 255, 255, 1);
   strokeWeight(2);
 
-  amp = new p5.Amplitude();
-
+  //create blinking and floating stars
   for (let i = 0; i < 200; i++) {
     stars.push({
       x: random(width),
@@ -38,16 +32,20 @@ function setup() {
 
 function draw() {
   background(0);
-  noStroke();
 
+  noStroke();
+  //update and draw all stars
   for (let s of stars) {
     s.x += s.speedX;
     s.y += s.speedY;
+
+    //wrap stars when they leave the screen
     if (s.x < 0) s.x = width;
     if (s.x > width) s.x = 0;
     if (s.y < 0) s.y = height;
     if (s.y > height) s.y = 0;
 
+    //star blinking effect
     s.opacity += s.blinkSpeed;
     if (s.opacity > 0.9 || s.opacity < 0.2) s.blinkSpeed *= -1;
 
@@ -55,31 +53,10 @@ function draw() {
     ellipse(s.x, s.y, s.r);
   }
 
-  let level = amp.getLevel() * 6;
-  smoothedLevel = lerp(smoothedLevel, level, 0.05);
-  let pulse = map(smoothedLevel, 0, 0.3, 0.95, 1.2);
-
+  //draw all saved lines
   for (let l of lines) {
-    let cx = centerX;
-    let cy = centerY;
-
-    let x1p = cx + (l.x1 - cx) * pulse;
-    let y1p = cy + (l.y1 - cy) * pulse;
-    let x2p = cx + (l.x2 - cx) * pulse;
-    let y2p = cy + (l.y2 - cy) * pulse;
-
-    drawingContext.shadowBlur = map(smoothedLevel, 0, 0.3, 10, 30);
-    drawingContext.shadowColor = `rgba(${l.r}, ${l.g}, ${l.b}, 0.6)`;
-
     stroke(l.r, l.g, l.b);
-    line(x1p, y1p, x2p, y2p);
-  }
-
-  if (drawing) {
-    let speed = dist(mouseX, mouseY, pmouseX, pmouseY);
-    let playbackRate = map(speed, 0, 50, 0.8, 1.3);
-    playbackRate = constrain(playbackRate, 0.8, 1.3);
-    song.rate(playbackRate);
+    line(l.x1, l.y1, l.x2, l.y2);
   }
 }
 
@@ -98,6 +75,7 @@ function mouseDragged() {
   let g = map(dy, -height / 2, height / 2, 50, 255);
   let b = map(radius, 0, sqrt(sq(width / 2) + sq(height / 2)), 80, 255);
 
+  //save the line into the array
   lines.push({ x1, y1, x2, y2, r, g, b });
 
   lastX = x1;
@@ -105,11 +83,6 @@ function mouseDragged() {
 }
 
 function mousePressed() {
-  if (!songStarted && song.isLoaded()) {
-    song.loop(0, 1, 0.3, 60);
-    amp.setInput(song);
-    songStarted = true;
-  }
   drawing = true;
   lastX = -999;
   lastY = -999;
